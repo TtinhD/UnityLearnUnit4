aab2e62c67973e03f5d7278d18768bf752613e36
Ttinhne <dongt1@roverkids.org>
2026-01-16 14:13:38 -0500

    Completed 4.4

--- GameplayMechanics/GameMechanics/Assets/Scripts/SpawnManager.cs
+++ GameplayMechanics/GameMechanics/Assets/Scripts/SpawnManager.cs
@@ -3,22 +3,38 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefabs;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int spawnWave;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
-
-        Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
+        SpawnEnemyWaves(spawnWave);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+
+        if (enemyCount == 0)
+        {
+            spawnWave++;
+            SpawnEnemyWaves(spawnWave);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
+    void SpawnEnemyWaves(int enemiesToSpawn)
+    {
+        for (int i = 0;i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition()
 
     {
